2f1404363c2a71e1b8dbcd1865664da7a5bf36e9
Wesley Bortoloso <wesleybortolosocco@gmail.com>
2025-03-16 23:44:00 -0300

    style: use sagu logos

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,9 +5,7 @@ export default function Login() {
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="bg-white md:w-1/2 flex flex-col items-center justify-center p-8 text-white">
         <div className="max-w-md text-center">
-          <img className="mb-20" src="../public/logo_ifc_extended.png" alt="" />
-          <h1 className="text-4xl font-bold mb-4 text-sagu-primary">SAGU</h1>
-          <p className="text-xl opacity-90 text-sagu-primary">Sistema de acompanhamento e gestão estudantil unificado</p>
+          <img src="logo_sagu_extended.png" alt="" />
         </div>
       </div>
 
