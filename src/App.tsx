e4dc8cc819098d3dcc7f90de660252bed24c82a9
Wesley Bortoloso <wesleybortolosocco@gmail.com>
2025-03-17 22:28:18 -0300

    feat: include home as new rote

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { AppLayout } from "./components/layout/AppLayout";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +11,11 @@ const App = () => (
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={
+            <AppLayout>
+              <Home />
+            </AppLayout>
+          } />
         </Routes>
     </BrowserRouter>
   </QueryClientProvider>
